2f4f39923ee09b0993d950d12feba46aa31ef834
Apprentice <apprentice@dbc12.local>
2013-11-11 18:43:20 -0800

    working game

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 1. http://socrates.devbootcamp.com/challenges/294
 2. http://socrates.devbootcamp.com/challenges/295
 3. http://socrates.devbootcamp.com/challenges/297
+
+Pseudo Code:
+(1) Create function which advances color to next pos
+(2)
--- racer_game.js
+++ racer_game.js
@@ -2,17 +2,35 @@ function updt_plyr_postion(player, pos) {
   // position goes from 0 to board_length-1;
 
   // ToDo: use .index() or .filter to refer to the #
-  var player_board = $( '#' + player + '_strip td' )
+  var player_board = $( '#' + player + '_strip td' );
+  // ToDo: refactor to use player_board in this line
   var board_length = $( '#' + player + '_strip td' ).length;
 
   if ( pos + 1 <= board_length ) {
     // this leaves a residual blank class behind
-    $( player_board[pos - 1]).removeClass("active")
-    $( player_board[pos]).addClass("active")
+    $( player_board[pos - 1]).removeClass("active");
+    $( player_board[pos]).addClass("active");
 
   } else {
-    alert("Game over!!")
+    alert("Game over!!");
   }
 
 }
 
+$(document).ready(function() {
+  var player1_pos = 0;
+  var player2_pos = 0;
+  console.log("Player 1 use z; Player 2 use /");
+  $(document).on('keypress', function(event) {
+    if ( event.which === 122 ) {
+      updt_plyr_postion("player1", player1_pos);
+      player1_pos = player1_pos + 1;
+    } else if ( event.which === 47 ) {
+      updt_plyr_postion("player2", player2_pos);
+      player2_pos = player2_pos + 1;
+    } else {
+      console.log( "Invalid key" + event.which );
+    }
+
+  });
+});
